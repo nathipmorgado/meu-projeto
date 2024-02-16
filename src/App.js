503b703628565f71d466bbc47c6619933d59e6df
nathy <nathaliapmorgado@gmail.com>
2024-02-16 16:01:27 -0300

    Adicionando meu projeto ao repositório

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome = "Ana"
 
   return (
     <div className="App">
-
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
     <SayMyName nome="Pedro" />
     <SayMyName nome="Conrado" />
     <SayMyName nome={nome} />
+   
     <Pessoa 
     nome="Nathália" 
     idade="27"
